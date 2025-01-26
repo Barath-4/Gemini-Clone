1218c32de5df871ca05c62cb3aabd17cd34c723b
barath <barathlatha2002@gmail.com>
2025-01-26 15:24:18 +0530

    Update gemini.js
    added api key

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -4,7 +4,7 @@ import {
     HarmBlockThreshold,
   } from "@google/generative-ai"
   
-  const apiKey = "Add Api Key Here";
+  const apiKey = "AIzaSyDke4Yltt0a7b-02kXdXKA5jbvfNyUkJKY";
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
